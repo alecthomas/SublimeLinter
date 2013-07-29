ec22699754a3a26d1bb6d31022f602510b55f775
Alec Thomas <alec@swapoff.org>
2013-07-29 11:15:28 -0400

    Factor Go linters out into classes.

--- sublimelinter/modules/go.py
+++ sublimelinter/modules/go.py
@@ -6,6 +6,7 @@ import os
 import re
 import subprocess
 from collections import namedtuple
+from distutils.spawn import find_executable
 
 from base_linter import BaseLinter, INPUT_METHOD_FILE
 
@@ -31,55 +32,107 @@ def parse_lines(view, filename, pattern, lines):
             yield match
 
 
-class Linter(BaseLinter):
-    pkg_path = '{GOPATH}/pkg/{GOOS}_{GOARCH}'
-    go_6g_pattern = re.compile(r'(.*?):(\d+): (.*)')
-    golint_pattern = re.compile(r'(.*?):(\d+):(\d+): (.*)')
+class BaseGoLinter(object):
+    binary = None
+    pattern = None
+    type = 'error'
 
-    def __init__(self, config):
-        super(Linter, self).__init__(config)
-        self._linters = [self.golint_check, self.go_6g_check]
+    def run(self, view, filename):
+        """Run linter, returning linter output. Each output line will be processed by apply()."""
+        raise NotImplementedError
+
+    @classmethod
+    def valid(cls):
+        cls.binary = find_executable(cls.binary)
+        if cls.binary is not None:
+            print 'GoLinter: %s enabled' % cls.__name__
+        return cls.binary is not None
+
+    def apply(self, view, filename, line):
+        match = self.pattern.match(line)
+        if match is None or not filename.endswith(match.group('filename')):
+            return None
+
+        groups = match.groupdict()
+        if 'position' in groups:
+            position = int(groups['position'])
+        else:
+            position = None
+        return GoError(line=int(groups['line_number']), position=position, type=self.type, message=groups['message'])
+
+
+class GoCompileLinter(BaseGoLinter):
+    binary = 'go'
+    pattern = re.compile(r'(?P<filename>.*?):(?P<line_number>\d+): (?P<message>.*)')
+    pkg_path = '{GOPATH}/pkg/{GOOS}_{GOARCH}'
+
+    def __init__(self):
         self._env = self._go_env('go')
 
-    def golint_check(self, view, filename):
-        errors = []
-        lines = run(['golint', filename])
-        for match in parse_lines(view, filename, self.golint_pattern, lines):
-            _, line_number, position, message = match.groups()
-            line_number = int(line_number)
-            line_point = view.text_point(line_number - 1, 0)
-            line_text = view.substr(view.line(line_point))
-            if '-golint' in line_text:
-                continue
-            errors.append(GoError(line=line_number, position=int(position), type='warning', message=message))
-        return errors
+    def run(self, view, filename):
+        return run(self._args(filename))
 
-    def go_6g_check(self, view, filename):
-        errors = []
-        lines = run(self._get_go_6g_args(filename))
-        for match in parse_lines(view, filename, self.go_6g_pattern, lines):
-            _, line_number, message = match.groups()
-            errors.append(GoError(line=int(line_number), position=None, type='error', message=message))
-        return errors
-
-    def _get_go_6g_args(self, filename):
+    def _args(self, filename):
         dir = os.path.dirname(filename)
         pkg_path = self.pkg_path.format(**self._env)
         # Find files in the current files package. Allows for split-package directories.
         file_package = self._package_for_file(filename)
         files = [file for file in glob.glob(dir + '/*.go')
                  if self._package_for_file(file) == file_package]
-        cmd = ['go', 'tool', '6g', '-o', '/dev/null', '-D', dir, '-I', pkg_path] + files
+        cmd = [self.binary, 'tool', '6g', '-o', '/dev/null', '-D', dir, '-I', pkg_path] + files
         print ' '.join(cmd)
         return cmd
 
+    def _package_for_file(self, filename):
+        with open(filename) as fd:
+            for line in fd:
+                if line.startswith('package'):
+                    return line.split()[1]
+
+    def _go_env(self, go):
+        env = {}
+        for line in run([go, 'env']):
+            k, v = line.split('=')
+            env[k] = v[1:-1].decode('string_escape')
+        return env
+
+
+class GolintLinter(BaseGoLinter):
+    binary = 'golint'
+    pattern = re.compile(r'(?P<filename>.*?):(?P<line_number>\d+):(?P<position>\d+): (?P<message>.*)')
+    type = 'warning'
+
+    def run(self, view, filename):
+        return run([self.binary, filename])
+
+    def apply(self, view, filename, line):
+        error = super(GolintLinter, self).apply(view, filename, line)
+
+        line_point = view.text_point(error.line - 1, 0)
+        line_text = view.substr(view.line(line_point))
+        if '-golint' in line_text:
+            return None
+
+        return error
+
+
+class Linter(BaseLinter):
+    linters = [GoCompileLinter, GolintLinter]
+
+    def __init__(self, config):
+        super(Linter, self).__init__(config)
+        self.linters = [linter() for linter in Linter.linters if linter.valid()]
+
     def built_in_check(self, view, code, filename):
         errors = []
-        for linter in self._linters:
+        for linter in self.linters:
             try:
-                errors.extend(linter(view, filename))
+                for line in linter.run(view, filename):
+                    error = linter.apply(view, filename, line)
+                    if error is not None:
+                        errors.append(error)
             except Exception as e:
-                print 'GoLinter: error: %s' % e
+                print '%s: error: %s' % (linter.__class__.__name__, e)
         return errors
 
     def parse_errors(self, view, errors, lines, errorUnderlines,
@@ -95,16 +148,3 @@ class Linter(BaseLinter):
             if error.position is not None:
                 self.underline_word(view, error.line, error.position - 1, underlines)
             self.add_message(error.line, lines, error.message, messages)
-
-    def _go_env(self, go):
-        env = {}
-        for line in run([go, 'env']):
-            k, v = line.split('=')
-            env[k] = v[1:-1].decode('string_escape')
-        return env
-
-    def _package_for_file(self, filename):
-        with open(filename) as fd:
-            for line in fd:
-                if line.startswith('package'):
-                    return line.split()[1]
